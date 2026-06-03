919f0102f358213be4b396f3fd25b7a22a8c3d0a
Jxsha1 <joshstevens10697@gmail.com>
2026-06-03 16:27:19 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,14 @@ type Runtime = import('@astrojs/cloudflare').Runtime<{
   DB: D1Database;
   SESSION: KVNamespace;
   INVOICE_VAULT: R2Bucket;
+  EMAIL: {
+    send: (params: {
+      to: { email: string; name?: string }[];
+      from: { email: string; name?: string };
+      subject: string;
+      text: string;
+    }) => Promise<void>;
+  };
 }>;
 
 declare namespace App {
