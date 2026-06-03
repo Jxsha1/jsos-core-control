e1b379c958ff8e9d83b01d481c56ad40a38fec0e
Jxsha1 <joshstevens10697@gmail.com>
2026-06-03 16:40:34 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,18 +1,9 @@
 /// <reference types="astro/client" />
 
 type Runtime = import('@astrojs/cloudflare').Runtime<{
-  // Strictly explicitly defines the infrastructure types passed into Astro context locals
   DB: D1Database;
   SESSION: KVNamespace;
   INVOICE_VAULT: R2Bucket;
-  EMAIL: {
-    send: (params: {
-      to: { email: string; name?: string }[];
-      from: { email: string; name?: string };
-      subject: string;
-      text: string;
-    }) => Promise<void>;
-  };
 }>;
 
 declare namespace App {
